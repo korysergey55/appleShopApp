fd1be2fb64a93343355e6d692fa6a2f969fce4d1
Serhiy Korytskyy <korysergey55@gmail.com>
2021-11-19 23:32:05 +0200

    flex-styles-filter

--- src/Components/filter/Filter.js
+++ src/Components/filter/Filter.js
@@ -7,26 +7,28 @@ import styles from "./styles.module.scss";
 
 const Filter = () => {
 
- const filter = useSelector(filterSelector);
- const dispatch = useDispatch();
+  const filter = useSelector(filterSelector);
+  const dispatch = useDispatch();
 
- const onHandleChange = (event) => {
-  const felterInputValue = event.target.value;
-  dispatch(setFilter(felterInputValue));
- };
- 
- return (
-  <label className={styles.filterLabel}>
-   Find Product
-   <input
-    className={styles.filterInput}
-    type="text"
-    value={filter}
-    onChange={onHandleChange}
-    placeholder="Enter somsing heare"
-   />
-  </label>
- );
+  const onHandleChange = (event) => {
+    const value = event.target.value;
+    dispatch(setFilter(value));
+  };
+
+  return (
+    <div className={styles.container}>
+      <label className={styles.label}>
+        Find Goods
+        <input
+          className={styles.input}
+          type="text"
+          value={filter}
+          onChange={onHandleChange}
+          placeholder="Enter something ..."
+        />
+      </label>
+    </div>
+  );
 };
 
 export default Filter;
